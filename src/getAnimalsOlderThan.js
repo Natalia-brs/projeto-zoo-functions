ce540ac9be0c6ade9cfe7b8b0beedbb5e0a50029
natalia <ntla.brs@gmail.com>
2022-10-31 19:19:44 -0300

    Requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,6 +4,4 @@ const data = require('../data/zoo_data');
 const getAnimalsOlderThan = (animals, ages) => species.filter(({ name }) => name === animals)[0]
   .residents.every(({ age }) => age > ages);
 
-console.log(getAnimalsOlderThan('lions', 1));
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,6 +4,4 @@ const { species } = data;
 
 const getSpeciesByIds = (...ids) => species.filter(({ id }) => ids.includes(id));
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-
 module.exports = getSpeciesByIds;
