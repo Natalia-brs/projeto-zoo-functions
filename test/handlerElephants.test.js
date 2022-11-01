832843ef349ca83cec83aa7f571806f7d624987d
natalia <ntla.brs@gmail.com>
2022-10-31 23:18:15 -0300

    lint da 5 e requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,5 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-    it
+  it('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
 });
