09a3e647b6243c62eee171245ef605687530f076
natalia <ntla.brs@gmail.com>
2022-11-01 17:52:20 -0300

    Req 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees } = data;
+
+const employName = (employeeName) => {
+  const find = employees.find(({ firstName, lastName }) => employeeName === firstName
+  || employeeName === lastName);
+
+  if (employeeName === find) {
+    throw new Error('Informações inválidas');
+  }
+  return find;
+};
+
+console.log(employName('01422318-ca2d-46b8-b66c-3e9e188244ed'));
+
+const getEmployeesCoverage = () => {
+
+};
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees } = data;
+const { species } = data;
+
+const getOldestFromFirstSpecies = (id) => {
+  const findAnimal = employees.find((employ) => employ.id === id).responsibleFor[0];
+  const old = species.find((especie) => especie.id === findAnimal).residents
+    .reduce((acc, velho) => (acc.age > velho.age ? acc : velho));
+  return [old.name, old.sex, old.age];
+};
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
