4e14b0d7dff106e7f49425151f0549b6c2392f4a
natalia <ntla.brs@gmail.com>
2022-11-02 14:19:38 -0300

    Por enquanto é isso

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,4 +4,6 @@ const data = require('../data/zoo_data');
 const getAnimalsOlderThan = (animals, ages) => species.filter(({ name }) => name === animals)[0]
   .residents.every(({ age }) => age > ages);
 
+// console.log((getAnimalsOlderThan('lions', 25)))
+
 module.exports = getAnimalsOlderThan;
