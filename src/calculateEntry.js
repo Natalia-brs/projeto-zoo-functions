c6261f642f907dde9ec607c2dba9b4527411946c
natalia <ntla.brs@gmail.com>
2022-11-02 13:04:38 -0300

    organizando outro codigo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,31 +2,29 @@ const data = require('../data/zoo_data');
 
 const { prices } = data;
 
-// const entrants = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
 // Filtrei os elementos do array por idade e passei para o objeto com suas respectivas chaves
 
-const countEntrants = (entrants) => ({
-  child: entrants.filter((child) => child.age < 18).length,
-  adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
-  senior: entrants.filter((senior) => senior.age >= 50).length,
+const countEntrants = (pessoas) => ({
+  child: pessoas.filter(({ age }) => age < 18).length,
+  adult: pessoas.filter(({ age }) => age >= 18 && age < 50).length,
+  senior: pessoas.filter(({ age }) => age >= 50).length,
 });
 
-// console.log(countEntrants(entrants));
-
 // Fazendo as verificaçoes, multiplicação e soma dos valores
-const calculateEntry = (entrants) => {
-  if (entrants === undefined) {
+const calculateEntry = (ingressos) => {
+  if (ingressos === undefined) {
     return 0;
   }
-  if (Object.keys(entrants).length === 0) {
+  if (Object.keys(ingressos).length === 0) {
     return 0;
   }
 
@@ -37,6 +35,6 @@ const calculateEntry = (entrants) => {
   return multAdult + multChild + multSenior;
 };
 
-// console.log(calculateEntry(entrants))
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
